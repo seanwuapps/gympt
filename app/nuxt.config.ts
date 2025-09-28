8b2a103d50fc8ee6e2bc692c2128c0060bb05ad1
Sean Wu <seanwuapps@gmail.com>
2025-09-28 18:00:54 +1000

    feat: add Scalar API documentation viewer at /docs route

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    [
+      '@scalar/nuxt',
+      {
+        route: '/docs',
+        theme: 'modern',
+        openapi: {
+          url: '/api/openapi',
+        },
+      },
+    ],
+  ],
 })
